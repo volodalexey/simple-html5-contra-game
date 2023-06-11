767734baac8dcdcad486c8aaf7931b79933a1ed2
Alexey <volodalexey@gmail.com>
2023-06-11 22:57:34 +0300

    gh-pages

--- src/entities/EntityType.ts
+++
@@ -1,9 +0,0 @@
-export enum EntityType {
-  enemy = 'enemy',
-  enemyBullet = 'enemyBullet',
-  hero = 'hero',
-  heroBullet = 'heroBullet',
-  box = 'box',
-  powerupBox = 'powerupBox',
-  spreadgunPowerup = 'spreadgunPowerup'
-}
--- src/utils/logger.ts
+++
@@ -1,4 +0,0 @@
-import debug from 'debug'
-
-export const logApp = debug('contra-app')
-export const logLayout = debug('contra-layout')
